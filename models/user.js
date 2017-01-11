3895aace07fd8ebe27438f0f89fdf334406b139f
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-11 16:16:51 +0000

    fixed a couple of typos in model user.js, corrected a file name and added a postcode key for the map later

--- models/user.js
+++ models/user.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
 
-const userSchema = new mongoose.Schema {
+const userSchema = new mongoose.Schema({
   username: {type: String, unique: true, trim: true, required: true},
   email: {type: String, unique: true, trim: true, required: true},
-  passwordHash: {type: String, required: true}
+  passwordHash: {type: String, required: true},
+  homePostcode: {type: String, trim}
 }
 
 userSchema
