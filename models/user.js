c4717b40c4c9ef763be159eabda31b14fb518242
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-11 15:41:14 +0000

    added userSchema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,10 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
+
+const userSchema = new mongoose.Schema {
+  username: {type: String, unique: true, trim: true, required: true},
+  email: {type: String, unique: true, trim: true, required: true},
+  password: {type: String, required: true},
+  passwordHash: {type: String, required: true}
+}
