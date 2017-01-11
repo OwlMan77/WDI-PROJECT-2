4e8071e1f86f7e223ad6863c21a8f497caed08d6
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-11 15:12:47 +0000

    added lines to inclued most of our authentication packages in server.js

--- server.js
+++ server.js
@@ -6,3 +6,14 @@ const mongoose         = require('mongoose');
 const expressJWT       = require('express-jwt');
 
 const app              = express();
+const config           = require('./config/config.js');
+
+mongoose.connect(config.db);
+
+app.use(morgan('dev'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded( {extended: true}));
+app.use(cors);
+app.use(express.static(`${__dirname}/public`));
+
+app.listen(config.port, console.log(`Express is running on: ${config.port}`));
