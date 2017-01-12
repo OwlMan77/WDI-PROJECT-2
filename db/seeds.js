6ac2cde7a23c10e4c901e734c415d75e8dc9c6a8
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-12 22:27:44 +0000

    added save function for dummy seeds

--- db/seeds.js
+++ db/seeds.js
@@ -5,3 +5,26 @@ mongoose.connect(databaseURL);
 const Cinema = require('./models/cinema');
 
 Cinema.collection.drop();
+
+
+const cinema1 = new Cinema({
+  id: {type: String},
+  name: {type: String},
+  location: {type: String},
+  lisitngs: {type: Object}
+});
+
+const cinema2 = new Cinema({
+  id: {type: String},
+  name: {type: String},
+  location: {type: String},
+  lisitngs: {type: Object}
+});
+
+
+
+
+// id: {type: String},
+// name: {type: String},
+// location: {type: String},
+// lisitngs: {type: Object}
