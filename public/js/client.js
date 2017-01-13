459158d3f59f8c9f325e8487d03d053bcef57d8b
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-13 10:49:55 +0000

    added basic map

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,1 @@
-"use strict";console.log("runnign");
+"use strict";var googleMap={}||googleMap,google=google;googleMap.mapSetup=function(){var o=document.getElementById("map-canvas"),e={zoom:12,center:new google.maps.LatLng(51.506178,(-.088369)),mapTypeId:google.maps.MapTypeId.ROADMAP};this.map=new google.maps.Map(o,e)},$(googleMap.mapSetup.bind(googleMap));
