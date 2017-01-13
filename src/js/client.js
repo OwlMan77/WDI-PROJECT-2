937c81485949f217512c148d9d6f76843ce6ee85
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-13 09:54:43 +0000

    began implementing basic map

--- src/js/client.js
+++ src/js/client.js
@@ -1,2 +1,15 @@
-const googleMaps = {} || googleMaps;
+const googleMap = {} || googleMap;
 const google = google;
+
+googleMap.mapSetup = function() {
+  const canvas = document.getElementById('map-canvas');
+
+  const mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(51.506178,-0.088369),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  this.map = new google.maps.Map(canvas, mapOptions);
+};
+
+$(googleMap.mapSetup.bind(googleMap));
